bbe7badb19891cb48657d38e4b843f27388daa16
Michael Whitham <michaelawhitham@hotmail.co.uk>
2022-10-10 12:31:30 +0100

    adding in first get path with testing to retrieve categories array

---
+++ Controllers/getCategories.controller.js
@@ -0,0 +1,7 @@
+const { selectCategories } = require("../Models/selectCategories.models");
+
+exports.getCategories = (req, res) => {
+  selectCategories().then((categories) => {
+    res.status(200).send({ categories });
+  });
+};
