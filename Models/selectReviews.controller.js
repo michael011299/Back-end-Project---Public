340f9cd99c4a59f261f742ba5fef496916200e29
Michael Whitham <michaelawhitham@hotmail.co.uk>
2022-10-12 11:49:38 +0100

    refactoring

--- Models/selectReviews.controller.js
+++ Models/selectReviews.controller.js
@@ -1,36 +1,28 @@
-const { query } = require("express");
 const db = require("../db/connection");
 
 exports.selectReviews = (queryCategory) => {
-  return db
-    .query(
-      `SELECT reviews.*,
-      COUNT(comments.review_id) AS comment_count
-      FROM reviews
-      LEFT JOIN comments ON comments.review_id = reviews.review_id
-      GROUP BY reviews.review_id
-      ORDER BY created_at DESC;
-      `
-    )
-    .then((result) => {
-      const results = result.rows;
-      if (Object.keys(queryCategory).length === 0) {
-        return results;
-      }
-      let values;
-      if (Object.keys(queryCategory).length > 0) {
-        values = result.rows.filter((row) => {
-          if (row.category === queryCategory.category) {
-            return row;
-          }
-        });
-      }
-      if (values.length === 0) {
+  let queryStr = `
+  SELECT reviews.*,
+  COUNT(comments.review_id) AS comment_count
+  FROM reviews
+  LEFT JOIN comments ON comments.review_id = reviews.review_id
+  `;
+  if (queryCategory.category === undefined) {
+    queryStr += `GROUP BY reviews.review_id ORDER BY created_at DESC;`;
+    return db.query(queryStr).then((result) => {
+      return result.rows;
+    });
+  } else if (queryCategory.category) {
+    queryStr += `WHERE reviews.category = $1 GROUP BY reviews.review_id;`;
+    return db.query(queryStr, [queryCategory.category]).then((result) => {
+      if (result.rows.length !== 0) {
+        return result.rows;
+      } else {
         return Promise.reject({
           status: 400,
           msg: `Invalid input`,
         });
       }
-      return values;
     });
+  }
 };
